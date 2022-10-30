e74d6f81e45c95def9320f119f0b00ac21656720
Jabir Omar <jabiromar400@gmail.com>
2022-10-30 07:34:30 +0300

    Done

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,37 +1,3 @@
-require 'pry'
-
 class Triangle
-  
-  def initialize(x, y, z)
-    @x = x
-    @y = y
-    @z = z
-  end
-
-  def kind
-    if @x > 0 && @y > 0 && @z > 0 && @x + @y > @z && @y + @z > @x && @z + @x > @y
-        if @x == @y && @y == @z
-          return :equilateral
-        elsif @x == @y || @x == @z || @y == @z
-          return :isosceles
-        elsif @x != @y && @y != @z
-          return :scalene
-        end
-    else
-      begin
-        raise TriangleError
-      end
-    end
-  end
-
-  class TriangleError < StandardError
-    def message 
-      "Invalid triangle"
-    end
-  end
-
+  # write code here
 end
-
-t1 = Triangle.new(3, 6, 7)
-puts t1.kind
-t2 = Triangle.new(1, 0, 4)
